2d99b71a6452a4ef669ddb3d4e4f93ac52778236
Scott Quested <me@scottquested.com>
2022-10-03 13:20:29 +0100

    Update hash on files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -8,7 +8,7 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
 	mode: "production",
 	output: {
-		filename: "[name].js",
+		filename: "[name].[contenthash].js",
 		publicPath: "/",
 	},
 	plugins: [
